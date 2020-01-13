a47ad24b908c9cdde908fd7cfe4cb9234d55f73b
Rob Gil <dds@rem5.com>
2020-01-13 12:08:09 -0500

    Apply manual change for dev environment to gain access to keyvault from vmss nodes

--- terraform/providers/dev/keyvault.tf
+++ terraform/providers/dev/keyvault.tf
@@ -5,5 +5,5 @@ module "keyvault" {
   owner        = var.owner
   environment  = var.environment
   tenant_id    = var.tenant_id
-  principal_id = module.k8s.principal_id
+  principal_id = "f9bcbe58-8b73-4957-aee2-133dc3e58063"
 }
