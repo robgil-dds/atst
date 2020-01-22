dod-ccpo/atat

Add missing regex and validation for StringFields
This commit adds further validation for StringFields that were missing
it. This mostly amounted to being Regex patters and max lengths.

atst/forms/edit_user.py
@@ -9,22 +9,26 @@ from .forms import BaseForm
 from .data import SERVICE_BRANCHES
 from atst.models.user import User
 from atst.utils.localization import translate
+from wtforms.validators import Length
+from atst.forms.validators import IsNumber
 
 from .validators import Name, DateRange, PhoneNumber
 
 
 USER_FIELDS = {
     "first_name": StringField(
-        translate("forms.edit_user.first_name_label"), validators=[Name()]
+        translate("forms.edit_user.first_name_label"),
+        validators=[Name(), Length(max=100)],
     ),
     "last_name": StringField(
-        translate("forms.edit_user.last_name_label"), validators=[Name()]
+        translate("forms.edit_user.last_name_label"),
+        validators=[Name(), Length(max=100)],
     ),
     "email": EmailField(translate("forms.edit_user.email_label"), validators=[Email()]),
     "phone_number": TelField(
         translate("forms.edit_user.phone_number_label"), validators=[PhoneNumber()]
     ),
-    "phone_ext": StringField("Extension"),
+    "phone_ext": StringField("Extension", validators=[IsNumber(), Length(max=10)]),
     "service_branch": SelectField(
         translate("forms.edit_user.service_branch_label"), choices=SERVICE_BRANCHES
     ),

atst/forms/task_order.py
@@ -7,18 +7,23 @@ from wtforms.fields import (
     HiddenField,
 )
 from wtforms.fields.html5 import DateField
-from wtforms.validators import Required, Length, NumberRange, ValidationError, Regexp
+from wtforms.validators import (
+    Required,
+    Length,
+    NumberRange,
+    ValidationError,
+)
 from flask_wtf import FlaskForm
 from numbers import Number
+from atst.forms.validators import IsNumber, AlphaNumeric
 
 from .data import JEDI_CLIN_TYPES
 from .fields import SelectField
 from .forms import BaseForm, remove_empty_string
 from atst.utils.localization import translate
-from .validators import REGEX_ALPHA_NUMERIC
 from flask import current_app as app
 
-MAX_CLIN_AMOUNT = 1000000000
+MAX_CLIN_AMOUNT = 1_000_000_000
 
 
 def coerce_enum(enum_inst):
@@ -62,7 +67,10 @@ class CLINForm(FlaskForm):
         coerce=coerce_enum,
     )
 
-    number = StringField(label=translate("task_orders.form.clin_number_label"))
+    number = StringField(
+        label=translate("task_orders.form.clin_number_label"),
+        validators=[IsNumber(), Length(max=4)],
+    )
     start_date = DateField(
         translate("task_orders.form.pop_start"),
         description=translate("task_orders.form.pop_example"),
@@ -120,7 +128,7 @@ class AttachmentForm(BaseForm):
             Length(
                 max=100, message=translate("forms.attachment.filename.length_error")
             ),
-            Regexp(regex=REGEX_ALPHA_NUMERIC),
+            AlphaNumeric(),
         ],
     )
     object_name = HiddenField(
@@ -129,7 +137,7 @@ class AttachmentForm(BaseForm):
             Length(
                 max=40, message=translate("forms.attachment.object_name.length_error")
             ),
-            Regexp(regex=REGEX_ALPHA_NUMERIC),
+            AlphaNumeric(),
         ],
     )
     accept = ".pdf,application/pdf"
@@ -142,6 +150,7 @@ class TaskOrderForm(BaseForm):
     number = StringField(
         label=translate("forms.task_order.number_description"),
         filters=[remove_empty_string],
+        validators=[IsNumber(), Length(max=13)],
     )
     pdf = FormField(
         AttachmentForm,

robgil-dds/atst

atst/forms/edit_user.py
@@ -9,22 +9,26 @@ from .forms import BaseForm
 from .data import SERVICE_BRANCHES
 from atst.models.user import User
 from atst.utils.localization import translate
+from wtforms.validators import Length
+from atst.forms.validators import Number
 
 from .validators import Name, DateRange, PhoneNumber
 
 
 USER_FIELDS = {
     "first_name": StringField(
-        translate("forms.edit_user.first_name_label"), validators=[Name()]
+        translate("forms.edit_user.first_name_label"),
+        validators=[Name(), Length(max=100)],
     ),
     "last_name": StringField(
-        translate("forms.edit_user.last_name_label"), validators=[Name()]
+        translate("forms.edit_user.last_name_label"),
+        validators=[Name(), Length(max=100)],
     ),
     "email": EmailField(translate("forms.edit_user.email_label"), validators=[Email()]),
     "phone_number": TelField(
         translate("forms.edit_user.phone_number_label"), validators=[PhoneNumber()]
     ),
-    "phone_ext": StringField("Extension"),
+    "phone_ext": StringField("Extension", validators=[Number(), Length(max=10)]),
     "service_branch": SelectField(
         translate("forms.edit_user.service_branch_label"), choices=SERVICE_BRANCHES
     ),

atst/forms/task_order.py
@@ -7,18 +7,23 @@ from wtforms.fields import (
     HiddenField,
 )
 from wtforms.fields.html5 import DateField
-from wtforms.validators import Required, Length, NumberRange, ValidationError, Regexp
+from wtforms.validators import (
+    Required,
+    Length,
+    NumberRange,
+    ValidationError,
+)
 from flask_wtf import FlaskForm
-from numbers import Number
+import numbers
+from atst.forms.validators import Number, AlphaNumeric
 
 from .data import JEDI_CLIN_TYPES
 from .fields import SelectField
 from .forms import BaseForm, remove_empty_string
 from atst.utils.localization import translate
-from .validators import REGEX_ALPHA_NUMERIC
 from flask import current_app as app
 
-MAX_CLIN_AMOUNT = 1000000000
+MAX_CLIN_AMOUNT = 1_000_000_000
 
 
 def coerce_enum(enum_inst):
@@ -30,8 +35,8 @@ def coerce_enum(enum_inst):
 
 def validate_funding(form, field):
     if (
-        isinstance(form.total_amount.data, Number)
-        and isinstance(field.data, Number)
+        isinstance(form.total_amount.data, numbers.Number)
+        and isinstance(field.data, numbers.Number)
         and form.total_amount.data < field.data
     ):
         raise ValidationError(
@@ -62,7 +67,10 @@ class CLINForm(FlaskForm):
         coerce=coerce_enum,
     )
 
-    number = StringField(label=translate("task_orders.form.clin_number_label"))
+    number = StringField(
+        label=translate("task_orders.form.clin_number_label"),
+        validators=[Number(), Length(max=4)],
+    )
     start_date = DateField(
         translate("task_orders.form.pop_start"),
         description=translate("task_orders.form.pop_example"),
@@ -120,7 +128,7 @@ class AttachmentForm(BaseForm):
             Length(
                 max=100, message=translate("forms.attachment.filename.length_error")
             ),
-            Regexp(regex=REGEX_ALPHA_NUMERIC),
+            AlphaNumeric(),
         ],
     )
     object_name = HiddenField(
@@ -129,7 +137,7 @@ class AttachmentForm(BaseForm):
             Length(
                 max=40, message=translate("forms.attachment.object_name.length_error")
             ),
-            Regexp(regex=REGEX_ALPHA_NUMERIC),
+            AlphaNumeric(),
         ],
     )
     accept = ".pdf,application/pdf"
@@ -142,6 +150,7 @@ class TaskOrderForm(BaseForm):
     number = StringField(
         label=translate("forms.task_order.number_description"),
         filters=[remove_empty_string],
+        validators=[Number(), Length(max=13)],
     )
     pdf = FormField(
         AttachmentForm,