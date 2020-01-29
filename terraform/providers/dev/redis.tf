ffb29d45fc72d9c50a560e7d0da02979b0b06333
Rob Gil <dds@rem5.com>
2020-01-29 11:31:21 -0500

    Adds Redis logging to log analytics

--- terraform/providers/dev/redis.tf
+++ terraform/providers/dev/redis.tf
@@ -1,10 +1,11 @@
 module "redis" {
-  source      = "../../modules/redis"
-  owner       = var.owner
-  environment = var.environment
-  region      = var.region
-  name        = var.name
-  subnet_id   = module.vpc.subnet_list["redis"].id
-  sku_name    = "Premium"
-  family      = "P"
+  source       = "../../modules/redis"
+  owner        = var.owner
+  environment  = var.environment
+  region       = var.region
+  name         = var.name
+  subnet_id    = module.vpc.subnet_list["redis"].id
+  sku_name     = "Premium"
+  family       = "P"
+  workspace_id = module.logs.workspace_id
 }
